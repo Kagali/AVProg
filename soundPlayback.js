d7d8a14d33bb133c20dfc36a2859fba67604e9e4
Kagali <32572069+Kagali@users.noreply.github.com>
2019-09-24 09:17:37 +0200

    14.09.

---
+++ soundPlayback.js
@@ -0,0 +1,12 @@
+let context = new AudioContext();
+let drumpads = document.getElementsByClassName("drumpad");
+let sounds = [];
+let soundNodes = [];
+
+for (lat i = 0, i < 0; i++){
+    drumpads[i].addEventListener("mousedown", function().playSound[i]);
+}
+
+function playSound(i){
+    let sound = new Audio("../")
+}
